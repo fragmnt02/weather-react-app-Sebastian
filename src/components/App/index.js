d51a449d4fbe97a0d3192602118441bc65a26d89
Paco <franciscoa@proscai.com>
2019-03-20 19:11:50 -0600

    logica form

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container } from './App.style';
 import { createGlobalStyle } from 'styled-components'
 import Form from '../Form';
+import gql from "graphql-tag";
+import { withApollo } from 'react-apollo';
+const GET_WEATHER = gql`
+query($lat:String!, $long:String!){
+  getWeather(lat:$lat, long:$long){
+    currently{
+      summary,
+      temperature,
+      icon
+    },
+    hourly{
+      summary,
+      icon,
+      data{
+        summary,
+        temperature,
+        icon
+    	}
+    },
+    daily{
+      summary,
+      icon,
+      data{
+        summary,
+        temperature,
+        icon
+    	}
+    }
+  }
+}
+`
 const GlobalStyle = createGlobalStyle`
   body {
     padding:0;
@@ -9,15 +40,51 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-function App() {
-  return (
-    <React.Fragment>
-      <GlobalStyle />
-      <Container>
-        <Form/>
-      </Container>
-    </React.Fragment>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      lat:'',
+      long:'',
+      loading:false
+    }
+  }
+
+  setCords=(lat,long)=>this.setState({lat,long},this.getWeather)
+
+  setLoading=(loading,callback)=>this.setState({loading},callback)
+
+  getWeather=()=>{
+    const { lat, long } = this.state;
+    const { client } = this.props;
+    client.query({
+      query: GET_WEATHER,
+      variables: {
+          lat,
+          long
+      }
+  })
+      .then(res => {
+          console.log(res.data)
+      })
+      .catch(error => alert(error.message))
+
+  }
+
+  render(){
+    const propsForm={
+      setCords:this.setCords,
+      setLoading:this.setLoading
+    }
+    return (
+      <React.Fragment>
+        <GlobalStyle />
+        <Container>
+          <Form {...propsForm} />
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+export default withApollo(App);
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,23 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyledPaper, StyledInput } from './Form.style';
 import IconButton from '@material-ui/core/IconButton';
 import Send from '@material-ui/icons/Send';
 import GpsFixed from '@material-ui/icons/GpsFixed';
+import gql from "graphql-tag";
+import { withApollo } from 'react-apollo';
+const GET_CORDS = gql`
+query($query:String!){
+    getCoords(query:$query){
+      features{
+        geometry{
+          coordinates
+        }
+      }
+    }
+  }
+`
 
-function App() {
-    return (
-        <React.Fragment>
-            <StyledPaper>
-                <IconButton>
-                    <GpsFixed />
-                </IconButton>
-                <StyledInput />
-                <IconButton>
-                    <Send />
-                </IconButton>
-            </StyledPaper>
-        </React.Fragment>
-    );
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    hadleChage = event => this.setState({ query: event.target.value })
+
+    hanndleEnter = event => {
+        if (event.key === 'Enter') this.handleSubmit();
+    }
+
+    handleSubmit = () => {
+        const { query } = this.state;
+        if (query !== '') {
+            const { client, setCords, setLoading } = this.props;
+            setLoading(true,()=>{
+                client.query({
+                    query: GET_CORDS,
+                    variables: {
+                        query
+                    }
+                })
+                    .then(res => {
+                        const lat = res.data.getCoords.features[0].geometry.coordinates[1]
+                        const long = res.data.getCoords.features[0].geometry.coordinates[0]
+                        setCords( lat, long );
+                    })
+                    .catch(error => setLoading(false,()=>alert(error.message)))
+            })
+        }
+    }
+
+    render() {
+        const { query } = this.state;
+
+        const propsInput = {
+            onChange: this.hadleChage,
+            value: query
+        }
+
+        const propsButton = {
+            onClick: this.handleSubmit
+        }
+
+        const propsPaper = {
+            onKeyUp: this.hanndleEnter
+        }
+
+        return (
+            <React.Fragment>
+                <StyledPaper {...propsPaper}>
+                    <IconButton>
+                        <GpsFixed />
+                    </IconButton>
+                    <StyledInput {...propsInput} />
+                    <IconButton {...propsButton}>
+                        <Send />
+                    </IconButton>
+                </StyledPaper>
+            </React.Fragment>
+        );
+    }
 }
 
-export default App;
+export default withApollo(Form);
